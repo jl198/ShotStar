c6035822a922cd3e8b32c6d02409964ab4505171
Jacob Leiner <jacob.leiner@gmail.com>
2022-02-11 03:31:29 -0800

    added basic db functionality with initial classes user and game

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,30 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Jake'}
+    games = [
+        {
+            'date': '12/01/2021',
+            'score': 25
+        },
+        {
+            'date': '12/11/2021',
+            'score': 24
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, games=games)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
